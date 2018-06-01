ea305ea764317c6a09d57e20fe911ec8ef24f4ed
Sydney Barlow <sydneybarlow44@gmail.com>
2018-06-01 15:15:11 +0000

    automatically backed up by learn

--- lib/hello.rb
+++ lib/hello.rb
@@ -8,8 +8,10 @@ def hello_t(array)
     end
     
     array
-    
+    else
+    puts "Hey! No block was given!"
   end
+end
   
   hello_t(["Tim", "Tom", "Jim"]) do |name|
     if name.start_with?("T")
